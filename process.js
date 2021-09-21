85a94dc38dab0126348252b6ea3f04a07dedb3fb
luipalencia <luipalencia@gmail.com>
2021-09-21 12:12:28 -0300

    edit details of the validator function

--- process.js
+++ process.js
@@ -2,120 +2,97 @@ const fs = require('fs');
 const { promisify } = require('util');
 
 const validateKeys = (file) => {
+    const DDMMAA = /^([0-2][0-9]|3[0-1])(\/|-)(0[1-9]|1[0-2])\2(\d{4})$/;
+    const MMDDAA = /^(?:0?[1-9]|1[1-2])([\-/.])(3[01]|[12][0-9]|0?[1-9])\1\d{4}$/;
     file = JSON.parse(file); 
 
     const isValidID = () => {
         if(file.id.length == 36 && file.id != undefined && file.id != null) {
-            return file.id;
-        } else {
-            return false;
+            return true;
         }
+            return false;
+        
     }
     const isValidTitle = () => {
         if(file.title.length <= 255 && file.title != undefined && file.title != null != null && file.title != '') {
-            return file.title;
-        } else {
+            return true;
+        } 
             return false;
-        }
+        
     }
     
     const isValidAuthor= () => {
         if(file.author && file.author.length <= 100 && file.author != undefined && file.author != null && file.author != '') {
-            return file.author;
-        } else {
+            return true;
+        } 
             return false;
-        }
+        
     }
     
     const isValidModifiedAt= () => {
-        const DDMMAA = /^([0-2][0-9]|3[0-1])(\/|-)(0[1-9]|1[0-2])\2(\d{4})$/;
-        const MMDDAA = /^(?:0?[1-9]|1[1-2])([\-/.])(3[01]|[12][0-9]|0?[1-9])\1\d{4}$/;
         if( DDMMAA.test(file.modifiedAt) == true || MMDDAA.test(file.modifiedAt) == true && file.modifiedAt != undefined && file.modifiedAt != null && file.modifiedAt != '') {
-            return file.modifiedAt;
-        } else {
+            return true;
+        } 
             return false;
-        }
+        
     }
     
     const isValidPublishedAt= () => {
-        const DDMMAA = /^([0-2][0-9]|3[0-1])(\/|-)(0[1-9]|1[0-2])\2(\d{4})$/;
-        const MMDDAA = /^(?:0?[1-9]|1[1-2])([\-/.])(3[01]|[12][0-9]|0?[1-9])\1\d{4}$/;
         if( DDMMAA.test(file.publishedAt) == true || MMDDAA.test(file.publishedAt) == true && file.publishedAt != undefined && file.publishedAt != null && file.publishedAt != '') {
-            return file.publishedAt;
-        } else {
+            return true;
+        } 
             return 'no ha sido publicado aun';
-        }
+        
     }
     
     const isValidURL= () => {
         if(file.url.includes('http') && file.url != undefined && file.url != null && file.url != '') {
-            return file.url;
-        } else {
+            return true;
+        } 
             return false;
-        }
+        
     }
     
     const isValidKeywords= () => {
         if(Array.isArray(file.keywords) == true && file.keywords.length <= 3 && file.keywords.length >= 1 && file.keywords != undefined && file.keywords != null && file.keywords != '') {
-            return file.keywords;
-        } else {
+            return true;
+        } 
             return false;
-        }
+        
     } 
     
     const isValidReadMins= () => {
         if(file.readMins <= 20 && file.readMins >= 1 && file.readMins!= undefined && file.readMins!= null) {
-            return file.readMins;
-        } else {
+            return true;
+        } 
             return false;
-        }
+        
     } 
     
     const isValidSource= () => {
         if(file.source == 'ARTICLE' || file.source == 'BLOG' || file.source == 'TWEET' || file.source == 'NEWSPAPER') {
-            return file.source;
-        } else {
+            return true;
+        } 
             return false;
-        }
+    
     }
 
-    return {
-        id: isValidID(),
-        title: isValidTitle(),
-        author: isValidAuthor(),
-        modifiedAt: isValidModifiedAt(),
-        publishedAt: isValidPublishedAt(),
-        url: isValidURL(),
-        keywords: isValidKeywords(),
-        readMins: isValidReadMins(),
-        source: isValidSource(),
+    if (
+        isValidID(file) &&
+        isValidTitle(file) &&
+        isValidAuthor(file) &&
+        isValidModifiedAt(file) &&
+        isValidPublishedAt(file) &&
+        isValidURL(file) &&
+        isValidKeywords(file) &&
+        isValidReadMins(file) &&
+      isValidSource(file)
+    ) {
+        return (true + ' => "' + file.title + '" ')
+    } else {
+        return (false + ' => "' + file.title + '" ')
     }
 }
-const propertiesArray = ["id", "title", "url", "keywords", "modifiedAt", "author", "readMins", "source"];
-let checker = (arr, target) => target.every(v => arr.includes(v));
-
-const validator = (data) => {
-
-    const resultsTrue = checker(data, propertiesArray);
-
-    if (resultsTrue === true) {
-    return validateKeys(data)
-    
-    } else {
-    return false;
-    }
-};
-
-// const validateFalse = (obj) => {
-// Object.keys(obj).map((key) => {
-// if(obj[key] === false) {
-//    console.log(false)
-// } else {
-//     console.log(true)
-// }
-// })
-// return obj;
-// }
 
 const promisifiedReadDir = promisify(fs.readdir);
 const promisifiedReadFile = promisify(fs.readFile);
@@ -128,8 +105,7 @@ async function main() {
              fileContent = await promisifiedReadFile(`./database/${file}`, { 
                 encoding: 'utf-8',
             }); 
-           // console.log(validateFalse(validator(fileContent)))
-            console.log(validator(fileContent))
+            console.log(validateKeys(fileContent))
         }
     } catch (err) {
         console.log(err)
