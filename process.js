dfd3dc210f21f46998d8d1019d7866662e5a5c91
luipalencia <luipalencia@gmail.com>
2021-09-22 10:03:21 -0300

    fix yup validator details

--- process.js
+++ process.js
@@ -8,10 +8,13 @@ let schema = yup.object().shape({
     title: yup.string().max(255).required(),
     author: yup.string().max(100).strict().required(),
     modifiedAt: yup.date().required(),
-    publishedAt: yup.date(),
-    url: yup.string().url().required(),
+    publishedAt: yup.date().nullable(),
+    url: yup.string().url().when("publishedAt", {
+       is: (publishedAt) => !publishedAt,
+       then: yup.string().url().matches(/https/).required()
+   }),
     keywords: yup.array().min(1).max(3).required(),
-    readMins: yup.number().required().positive().integer(),
+    readMins: yup.number().required().min(1).max(20).positive().integer(),
     source: yup.mixed().oneOf(['BLOG', 'ARTICLE', 'NEWSPAPER', 'TWEET']).required(),
   });
 
